0866b243855b9f2d0a0622f7fb98639860f68f23
Matijaz <matijaz69@gmail.com>
2021-02-22 11:15:24 +0100

    Add main program and example input data

--- SumFinder/Program.cs
+++ SumFinder/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SumFinder
 {
@@ -10,6 +6,20 @@ namespace SumFinder
     {
         static void Main(string[] args)
         {
+            //var numbers = new List<decimal>() { 3.1m, 9, 8, 4, 5, 7, 10 };
+            string filePath = ReadInput.InputFilePath;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Odczytuję dane z pliku: {filePath}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            var input = new ReadInput().ParseNumbersFile(filePath);
+            if (input.Count > 1)
+            {
+                decimal target = input[0];
+                input.RemoveAt(0);
+                new SumFinder().FindSums(input, target);
+            }
+            Console.WriteLine("Naciśnij Enter");
+            Console.ReadLine();
         }
     }
 }
